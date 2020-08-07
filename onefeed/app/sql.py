d8810efbcc78ec0df3a098ccf4b1c83c4320f779
anorakj <nicholasjcc@gmail.com>
2020-08-07 11:33:38 +0800

    order by updatetime

--- onefeed/app/sql.py
+++ onefeed/app/sql.py
@@ -2,7 +2,7 @@
 
 
 GET_MESSAGE = """SELECT store.id, message_info FROM sync JOIN store ON sync.message_id = store.id 
-                 WHERE json_extract(store.message_info, '$.source') = ?"""
+                 WHERE json_extract(store.message_info, '$.source') = ? ORDER BY sync.updatetime desc"""
 DELETE_MESSAGE = """ DELETE FROM sync
                      WHERE id  IN ( SELECT id FROM (SELECT sync.id, ROW_NUMBER() OVER(ORDER BY sync.update_time desc) r
                      FROM sync JOIN store ON sync.message_id = store.id 
