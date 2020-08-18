d212b3e27e80e5a1fca81eed33d4622a2f92ded7
anorakj <nicholasjcc@gmail.com>
2020-08-18 16:16:41 +0800

    add infoq frontend

--- onefeed/retriever/infoq/spiders/articles.py
+++ onefeed/retriever/infoq/spiders/articles.py
@@ -14,13 +14,14 @@ class ArticlesSpider(scrapy.Spider):
         headers = {'origin': 'https://www.infoq.cn', 'referer': 'https://www.infoq.cn', 'host': 'www.infoq.cn'}
         return [scrapy.Request("https://www.infoq.cn/public/v1/my/recommond", headers=headers, method='POST', body=json.dumps({'size': 12})),
                 scrapy.Request("https://www.infoq.cn/public/v1/my/recommond", headers=headers, method='POST',
-                               body=json.dumps({'size': 12, 'score': 1e3 * int(time.time() - 4 * 3600)}))
+                               body=json.dumps({'size': 12, 'score': int(1e3 * (time.time() - 12 * 3600))}))
                 ]
 
     def parse(self, response):
         for article in response.json()['data']:
             item = ArticlesItem()
             item['title'] = article['article_title']
+            print(item['title'])
             item['link'] = "https://www.infoq.cn/article/{}".format(article['uuid'])
             item['description'] = article['article_summary']
             item['source'] = 'infoq_articles'
